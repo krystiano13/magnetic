1dbad007782ec256f23fab9cbfd7120488247e51
krystiano13 <crystianotv@gmail.com>
2023-07-26 11:18:45 +0200

    adding email to database

--- send.php
+++ send.php
@@ -5,6 +5,36 @@
     }
 
     else {
-        echo "<h4>Email successfull added to our newsletter !!!</h4>";
+
+        $user = 'root';
+        $host = 'localhost';
+        $passwd = '';
+        $db = 'magnetic';
+
+        $email = $_POST['email'];
+
+        $connection = mysqli_connect($host,$user,$passwd,$db);
+
+        if($connection) {
+            //check if email not exists
+            $sql1 = "SELECT email FROM emails WHERE email = '$email'";
+            $query = mysqli_query($connection,$sql1);
+
+            if(mysqli_num_rows($query) < 1) {
+
+                $sql2 = "INSERT INTO emails VALUES(NULL,'$email')";
+                
+                if(mysqli_query($connection,$sql2)) {
+                    echo "<h4>Email successfull added to our newsletter !!!</h4>";
+                }
+                else {
+                    echo "<h4>Error !!! We can't add your email now</h4>";
+                }
+         
+            }
+            else {
+                echo "<h4>We already have your email !!!</h4>";
+            }
+        }
     }
         
